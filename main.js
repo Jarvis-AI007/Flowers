46e878fc617c089edc882c3f39cd792d9abf494b
Jarvis-AI007 <pankajsharma9596708685@gmail.com>
2024-05-06 00:14:49 +0530

    done

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-onload = () => {
-    const c = setTimeout(() => {
-      document.body.classList.remove("not-loaded");
-      clearTimeout(c);
-    }, 1000);
-  };
+// Function to display the "Good Morning" text
+function displayGoodMorning() {
+  alert("Good Morning DNC !! 😉 ... wanna see some flowers for uhh ?? 💕 ");
+}
+
+// Wait for the DOM content to be loaded
+window.onload = () => {
+  // Display the "Good Morning" text
+  displayGoodMorning();
+
+  // Remove the "not-loaded" class after 1000 milliseconds
+  setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+  }, 1000);
+};
